7c47858919b909959be2635f0f582e2c2931d62b
cordis-dev <darius@adroiti.com>
2022-01-05 11:08:47 +0200

    Create t.ts

---
+++ t.ts
@@ -0,0 +1,7 @@
+// should not report trailing-comma
+// should report trailing-whitespace
+@NgModule({ 
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes, { enableTracing: true }) ],
+})
+export class AppModule { }
